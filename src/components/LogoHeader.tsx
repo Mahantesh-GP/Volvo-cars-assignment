84156df0b79d6f23e29575f2fed9924f3dffd6cb
shreyas bs <shreyasbs86@gmail.com>
2024-07-11 00:19:39 +0530

    react test completion

---
+++ src/components/LogoHeader.tsx
@@ -0,0 +1,13 @@
+export default function LogoHeader() {
+  return (
+    <div className="grid gap-x-16">
+      <header className="text-center">
+        <img
+          className="img-logo text-center justify-center "
+          src="https://www.volvocars.com/static/shared/images/volvo-wordmark-black.svg"
+          alt="Volvo Cars Web Design System"
+        ></img>
+      </header>      
+    </div>
+  );
+}
